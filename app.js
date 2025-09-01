f303b569b0bf2673378eca614cc22bd2e702d816
Gowtham S <116778691+GowthamSFSD@users.noreply.github.com>
2025-09-01 17:24:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello from Jenkins Deployment!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT,  () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
