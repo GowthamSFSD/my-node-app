2e950da5b501b6aa574d7826b7348f5dfe5d875f
Gowtham S <gowtham41722000@gmail.com>
2025-09-01 16:15:13 +0530

    chore: initialize package.json with express dependency

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 6000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Jenkins Deployment!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
